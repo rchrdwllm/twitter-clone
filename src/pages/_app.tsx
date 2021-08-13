5035a4b703915c4638bb55c87fc726a634a5dab1
Richard William <rchrdwllm19@gmail.com>
2021-08-13 19:14:31 +0800

    added store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import "../styles/fonts/style.css";
 import Layout from "./layout";
 import Router from "next/router";
 import { Provider } from "next-auth/client";
+import { Provider as StoreProvider } from "react-redux";
+import { createStore } from "redux";
+import { allReducers } from "../reducers";
 import { AppComponent } from "next/dist/next-server/lib/router/router";
 import ProgressBar from "@badrap/bar-of-progress";
 
@@ -17,12 +20,16 @@ Router.events.on("routeChangeStart", progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
+const store = createStore(allReducers as any);
+
 const App: AppComponent = ({ Component, pageProps }) => {
     return (
         <Provider session={pageProps.session}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <StoreProvider store={store}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </StoreProvider>
         </Provider>
     );
 };
